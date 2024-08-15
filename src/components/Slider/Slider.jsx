c0fd739c9ba3f4727690eaba0bd5ee5e92d4d534
saam <saamsallvin@gmail.com>
2024-08-15 16:32:42 +0530

    added: slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { sliderDatas } from "./sliderDatas.js";
 
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <Carousel>
+      {sliderDatas.map((sliderData, index) => (
+        <Carousel.Item key={index}>
+          <img className="carousel-image" src={sliderData.image} />
+          <Carousel.Caption className="carousel-caption">
+            <h1 className="mb-4">{sliderData.heading}</h1>
+            <a className="btn ">
+              <h6>Buy now</h6>
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 };
 
 export default Slider;
--- src/components/Slider/sliderDatas.js
+++ src/components/Slider/sliderDatas.js
@@ -1,8 +1,24 @@
+import Cake from "../../assets/images/cake-2.png";
+import Cookies from "../../assets/images/cookies.png";
+import Croissant from "../../assets/images/croissant.png";
+
 export const sliderDatas = [
   {
     id: 1,
-    image: "",
-    text: "Pastries upto 40% Off",
+    image: Cake,
+    heading: "Cake upto 50% Off",
+    buttonLink: "#",
+  },
+  {
+    id: 2,
+    image: Cookies,
+    heading: "Cookies upto 35% Off",
+    buttonLink: "#",
+  },
+  {
+    id: 3,
+    image: Croissant,
+    heading: "Croissant upto 30% Off",
     buttonLink: "#",
   },
 ];
